f373ad08a5e4d02d5a6b1d679668fb9628aca2ea
Fabrizio <fabrinucci99@gmail.com>
2023-03-29 19:50:23 -0300

    Add NextAuth authentication via GitHub and Google

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,13 @@
+import { SessionProvider } from 'next-auth/react';
 import type { AppProps } from 'next/app';
 import { Navbar } from 'components/navbar';
 import 'styles/globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <SessionProvider session={pageProps.session}>
       <Navbar />
       <Component {...pageProps} />
-    </>
+    </SessionProvider>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,14 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px',
+      '2xl': '1800px',
+    },
   },
   plugins: [],
 };
